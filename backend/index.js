02b2a46d992c13757d5f207a74e9acf4b3217103
Liza Ramo <liza.h.ramo@gmail.com>
2016-04-23 20:26:42 -0400

    moon phases & weather changes

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ var app = express();
 var getIss = require('./actions/getIss');
 var getAsteroids = require('./actions/getAsteroids');
 var getWeather = require('./actions/getWeather');
+var getMoonPhase = require('./actions/getMoonPhase');
 // var getMoonPhase = require('./actions/getIss');
 // var getPlanets = require('./actions/getIss');
 
@@ -18,41 +19,37 @@ app.use(bodyParser.urlencoded({extended: true}));
 var APP_ID = 'amzn1.echo-sdk-ams.app.d7e49c7e-64db-4d63-b758-f2ccc587eed9';
 var AlexaSkill = require('./AlexaSkill');
 
-var HistoryBuffSkill = function() {
+var WhatsOutsideSkill = function() {
   AlexaSkill.call(this, APP_ID);
 };
 
 // Extend AlexaSkill
-HistoryBuffSkill.prototype = Object.create(AlexaSkill.prototype);
-HistoryBuffSkill.prototype.constructor = HistoryBuffSkill;
-
-HistoryBuffSkill.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
-  console.log("HistoryBuffSkill onSessionStarted requestId: " + sessionStartedRequest.requestId
+WhatsOutsideSkill.prototype = Object.create(AlexaSkill.prototype);
+WhatsOutsideSkill.prototype.constructor = WhatsOutsideSkill;
+WhatsOutsideSkill.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+  console.log("WhatsOutsideSkill onSessionStarted requestId: " + sessionStartedRequest.requestId
       + ", sessionId: " + session.sessionId);
 
   // any session init logic would go here
 };
-
-HistoryBuffSkill.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-  console.log("HistoryBuffSkill onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+WhatsOutsideSkill.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+  console.log("WhatsOutsideSkill onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
   getWelcomeResponse(response);
 };
-
-HistoryBuffSkill.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+WhatsOutsideSkill.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
   console.log("onSessionEnded requestId: " + sessionEndedRequest.requestId
       + ", sessionId: " + session.sessionId);
-
   // any session cleanup logic would go here
 };
-
 function getWelcomeResponse(response) {
   var lat = -74.010074;
   var lng = 40.709160;
-
+  var userLocale = 'newyork,ny'
   Promise.all([
+    getMoonPhase(userLocale),
+    getWeather(lat, lng),
     getIss(lat, lng)
     // getAsteroids(lat, lng),
-    // getWeather(lat, lng)
   ])
   .then(function(values) {
     response.tell(values.join('\n'));
@@ -66,41 +63,36 @@ function getWelcomeResponse(response) {
 // Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
   // Create an instance of the HistoryBuff Skill.
-  var skill = new HistoryBuffSkill();
+  var skill = new WhatsOutsideSkill();
   skill.execute(event, context);
 };
 
 /* End Alexa */
 
+/* one route to rule them all (dev)*/
+app.get('/', function(req, res) {
+  res.header("Content-Type",'application/json');
 
+  // ASSUME WE GET LAT LNG INFORMATION FROM FRONTEND
 
+  var lat = -74.010074;
+  var lng = 40.709160;
+  var userLocale = 'newyork,ny'
 
+  Promise.all([
+    getWeather(lat, lng),
+    getMoonPhase(userLocale),
+    getIss(lat, lng)
+  ])
+  .then(function(values) {
+    res.json(values);
+  })
+  .catch(function(error){
+    console.log("SOMETHING WENT WRONG", error);
+  })
 
-//
-//
-// /* one route to rule them all */
-// app.get('/', function(req, res) {
-//   res.header("Content-Type",'application/json');
-//
-//   // ASSUME WE GET LAT LNG INFORMATION FROM FRONTEND
-//
-//   var lat = -74.010074;
-//   var lng = 40.709160;
-//
-//   Promise.all([
-//     getIss(lat, lng),
-//     // getAsteroids(lat, lng),
-//     getWeather(lat, lng)
-//   ])
-//   .then(function(values) {
-//     res.json(values);
-//   })
-//   .catch(function(error){
-//     console.log("SOMETHING WENT WRONG", error);
-//   })
-//
-// });
-//
-// app.listen(3000, function() {
-//   console.log('server started on port 3000');
-// });
+});
+
+app.listen(3000, function() {
+  console.log('server started on port 3000');
+});
